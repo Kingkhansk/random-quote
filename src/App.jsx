7155b44a09b0496e84587b865806d8227deb8141
kingkhansk <kingkhan340260@gmail.com>
2024-05-28 13:05:52 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import QuoteMachine from './components/quotemachine';
+
+function App() {
+  return (
+    <div className="App">
+
+      <QuoteMachine />
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/quotemachine.jsx
@@ -0,0 +1,42 @@
+
+import React, { useState, useEffect } from 'react';
+const QuoteMachine = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const fetchQuote = async () => {
+    try {
+      const response = await fetch('https://api.quotable.io/random');
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await response.json();
+      setQuote(data.content);
+      setAuthor(data.author);
+    } catch (error) {
+      console.error('Error fetching the quote', error);
+    }    
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  return (
+    <div id="quote-box">
+      <p id="text">{quote}</p>
+      <p id="author">- {author}</p>
+      <button id="new-quote" onClick={fetchQuote}>New Quote</button>
+      <a 
+        id="tweet-quote" 
+        href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${quote}" - ${author}`)}`} 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        Tweet
+      </a>
+    </div>
+  );
+};
+
+export default QuoteMachine;
